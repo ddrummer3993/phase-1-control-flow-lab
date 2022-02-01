41b3c9a2df76c964e744209e5feb83236421e17f
David Drummer <david.b.drummer@gmail.com>
2022-01-31 19:13:10 -0500

    commit tree 6db5c9b11b005a808be60954ea94f9ea593517c1

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(depth) {
+  let charge;
+  if (depth <= 400) {
+    charge = 'This one is on me!';
+  } else if (depth > 2000 && depth <= 2500) {
+      charge = 'I will gladly take your thirty bucks.';
+  } else if (depth > 2500) {
+      charge = 'No can do.';
+  };
+  return charge;
+};
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+};
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(amount){
+  let response;
+  switch (amount) {
+    case 'generous':
+      response = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      response = 'Thank you.';
+      break;
+    default:
+      response = 'Bye.';
+    };
+  return response;
+};
